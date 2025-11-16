3e308e4b6b0beed23a93b4359513d0881db35aa8
Larissa Vitória <166885238+LarissaVitoria05@users.noreply.github.com>
2025-11-15 21:21:50 -0300

    adicionando um carregamento dinâmico a HomePage
    Adicionando um Lazy Loading na Homepage, o id capturando o Place_Id e enviando para o componente DetailsPage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,22 @@
-import { Routes } from '@angular/router';
+    import { Routes } from '@angular/router';
 
-export const routes: Routes = [
+    export const routes: Routes = [
   {
     path: 'home',
+    // carregamento mais dinâmico (lazy loading) da homepage
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
-  
-
-  {
-    path: 'details/:id',
-    loadComponent: () => import('./details/details.page').then( m => m.DetailsPage)
-  },
-
-
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'details/:id', 
+    loadComponent: () => import('./details/details.page').then((m) => m.DetailsPage),
+   },
 ];
+
+  //  atividade da semana 3: página de detalhes
+  // o ":id" captura o place_id (id do local/evento)
+  // e envia para o componente DetailsPage.
